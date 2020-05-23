f4a8d146cd0aee22892cc2d2cda1c1fb694634d3
Jon Jagger <jon@jaggersoft.com>
2020-05-23 13:38:26 +0100

    Remove hidden_filenames from manifest; use trap handler in cyber-dojo.sh instead

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,5 +1,26 @@
 set -e
 
+# ------------------------------------------------------------------------
+# cyber-dojo returns text files under /sandbox that are
+# created/deleted/changed. In tidy_up you can remove any
+# such files you don't want returned to the browser.
+
+trap tidy_up EXIT
+
+function tidy_up()
+{
+  delete_dirs coverage
+}
+
+function delete_dirs()
+{
+  for dirname in "$@"
+  do
+      rm -rf "${dirname}" 2> /dev/null || true
+  done
+}
+
+# ------------------------------------------------------------------------
 # remove existing coverage report
 [ -e report/coverage.txt ] && rm report/coverage.txt
 
