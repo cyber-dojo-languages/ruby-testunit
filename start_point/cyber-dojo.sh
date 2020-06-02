7447a680f5f69e1456959e4f1f7d604d9635b0cc
Jon Jagger <jon@jaggersoft.com>
2020-06-02 12:36:33 +0100

    In cyber-dojo.sh use fs-cleaner scripts installed by runner

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,28 +1,23 @@
 set -e
 
-# ------------------------------------------------------------------------
-# cyber-dojo returns text files under /sandbox that are
-# created/deleted/changed. In tidy_up you can remove any
-# such files you don't want returned to the browser.
-
-trap tidy_up EXIT
-
-function tidy_up()
+# --------------------------------------------------------------
+# Text files under /sandbox are automatically returned...
+source ~/cyber_dojo_fs_cleaners.sh
+export REPORT_DIR=${CYBER_DOJO_SANDBOX}/report
+function cyber_dojo_enter()
 {
-  delete_dirs coverage
+  # 1. Only return _newly_ generated reports.
+  cyber_dojo_reset_dirs ${REPORT_DIR}
 }
-
-function delete_dirs()
+function cyber_dojo_exit()
 {
-  for dirname in "$@"
-  do
-      rm -rf "${dirname}" 2> /dev/null || true
-  done
+  # 2. Remove text files we don't want returned.
+  cyber_dojo_delete_dirs coverage # ...
+  #cyber_dojo_delete_files ...
 }
-
-# ------------------------------------------------------------------------
-# remove existing coverage report
-[ -e report/coverage.txt ] && rm report/coverage.txt
+cyber_dojo_enter
+trap cyber_dojo_exit EXIT SIGTERM
+# --------------------------------------------------------------
 
 # turn off colour for new coverage report
 export NO_COLOR=1
